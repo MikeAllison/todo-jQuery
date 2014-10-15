addf3a068bebbc0d56ab65b0c09b2e48b4d95404
MikeAllison <mikeallison79@gmail.com>
2014-10-15 10:21:41 -0400

    First commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	
+	//Bind click event handler to add button
+	$("#add-task-btn").bind("click", addTask);
+	
+	//FUNCTIONS
+	function addTask() {
+		var taskTextVal = $("#add-task-text").val();
+		
+		var taskListItem = "<li>";
+		taskListItem += "<input type='checkbox' class='checkbox'>";
+		taskListItem += "<label class='item'>" + taskTextVal + "</label>";
+		taskListItem += "<button class='edit'>Edit</button>";
+		taskListItem += "<button class='delete'>Delete</button>";
+		taskListItem += "</li>";
+		
+		//Append new item to incomplete list
+		$("#incomplete-list").append(taskListItem);
+		
+		//Bind click events to checkbox
+		$(".checkbox").bind("click", changeStatus);
+		$(".delete").bind("click", deleteListItem);
+		$(".edit").bind("click", editListItem);
+		
+		//Clear the textbox
+		$("#add-task-text").val('');
+	};
+	
+	function changeStatus() {
+		var taskListItem = $(this).parent();
+		var checkboxStatus = $(this).prop("checked");
+		
+		if (checkboxStatus === true) {
+			$("#completed-list").append(taskListItem);
+		} else {
+			$("#incomplete-list").append(taskListItem);
+		}
+	};
+	
+	function editListItem() {
+		var taskValue = $(this).siblings("label").text();
+		$(this).siblings("label").replaceWith("<input type='text' class='edit-item' value=" + taskValue + ">");
+		$(this).text("Save");
+		$(this).bind("click", saveListItem);
+	};
+	
+	function saveListItem() {
+		var taskValue = $(this).siblings(".edit-item").val();
+		$(this).text("Edit");
+		$(this).bind("click", editListItem);
+		$(this).siblings(".edit-item").replaceWith("<label class='item'>" + taskValue + "</label>");
+	};
+	
+	function deleteListItem() {
+		$(this).parent().remove();
+	};
+	
+}); //End ready
